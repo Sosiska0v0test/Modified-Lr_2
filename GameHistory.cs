c17978dc94924e5aeeb06e07f43811b12c063833
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2023-10-25 15:42:38 +0300

    Add files via upload

---
+++ GameHistory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    class GameHistory
+    {
+        private BasicAccount FirstPlayer;
+        private BasicAccount SecondPlayer;
+        private static int Games = 0;
+        private static int temp = 0;
+        private int GameID;
+        private bool IsFirstPlayerWon;
+        private int RatingForGame;
+
+        public GameHistory(BasicAccount firstPlayer, BasicAccount secondPlayer, bool isFirstPlayerWon, int ratingForGame)
+        {
+            FirstPlayer = firstPlayer;
+            SecondPlayer = secondPlayer;
+            IsFirstPlayerWon = isFirstPlayerWon;
+            RatingForGame = ratingForGame;
+            if (temp % 2 == 1)
+                Games--;
+            GameID = Games;
+            Games++;
+            temp++;
+        }
+
+        public void ShowPlayerHistory()
+        {
+            Console.WriteLine("Game ID: " + GameID + "| Opponent is " + SecondPlayer.playerName +
+            ". Winner of the game is " + (IsFirstPlayerWon ? FirstPlayer.playerName : SecondPlayer.playerName) +
+            ". Played for " + RatingForGame + " points");
+        }
+        public void ShowPlayerHistoryReverse()
+        {
+            Console.WriteLine("Game ID: " + GameID + "| Opponent is " + FirstPlayer.playerName +
+            ". Winner of the game is " + (!IsFirstPlayerWon ? FirstPlayer.playerName : SecondPlayer.playerName) +
+            ". Played for " + RatingForGame + " points");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using LR2;
+
+namespace LR2
+{
+    public class LR2
+    {
+        public static void Main(String[] args)
+        {
+            WinStreakAccount Player_1 = new WinStreakAccount("Sofia");
+            PremiumAccount Player_2 = new PremiumAccount("Liza");
+            Console.WriteLine(Player_1.playerName + " - WinStreak Account\n" + Player_2.playerName + " - Premium Account");
+            //3 basic games
+            CreateGame.StartGame(CreateGame.CreateBasicGame(Player_1, Player_2, 10, Victory.Player1));
+            CreateGame.StartGame(CreateGame.CreateBasicGame(Player_1, Player_2, 20, Victory.Player1));
+            
+
+            CreateGame.StartGame(CreateGame.CreateBasicGame(Player_1, Player_2, 50, Victory.Player1));
+            CreateGame.StartGame(CreateGame.CreateBasicGame(Player_1, Player_2, 5, Victory.Player1));
+            //training game with out points
+            CreateGame.StartGame(CreateGame.CreateTrainingGame(Player_1, Player_2, Victory.Player2));
+            CreateGame.StartGame(CreateGame.CreateBasicGame(Player_1, Player_2, 5, Victory.Player2));
+            //all-in-game
+            CreateGame.StartGame(CreateGame.CreateAllInGame(Player_1, Player_2, Victory.Player1));
+
+
+            Player_1.GetStats();
+            Player_2.GetStatsReverse();
+        }
+    }
+}
