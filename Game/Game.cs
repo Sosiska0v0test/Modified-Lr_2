122d76216dec2b6ac8e4b87e0fedda36d438ec93
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2023-10-25 15:40:59 +0300

    Add files via upload

---
+++ Game/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    enum Victory { Player1, Player2 }
+    enum Players { Player1, Player2 }
+
+    internal abstract class Game
+    {
+        public BasicAccount Player1 { get;  set; }
+        public BasicAccount Player2 { get;  set; }
+        public int RatingForGame { get; set; }
+        public Victory Winner { get;  set; }
+
+
+        public abstract int GetRatingForGame(Players player);
+    }
+}
---
+++ Game/TrainingGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    internal class TrainingGame : Game
+    {
+        public TrainingGame(BasicAccount player1, BasicAccount player2, Victory winner)
+        {
+            Player1 = player1;
+            Player2 = player2;
+            Winner = winner;
+            RatingForGame = 0;
+        }
+        public override int GetRatingForGame(Players player)
+        {
+            //rating is not allowed in training game
+            return RatingForGame;
+        }
+    }
+}
