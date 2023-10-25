d1a1eea06d363da2c7a46a920a7f1cb55b1de11a
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2023-10-25 15:42:14 +0300

    Add files via upload

---
+++ Account/PremiumAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    class PremiumAccount : BasicAccount
+    {
+        public PremiumAccount(string playerName) : base(playerName) { }
+        //premium account loses half as many points
+
+        public override void LoseGame(Game game)
+        {
+            gamesCount++;
+            int ratingForGame = game.GetRatingForGame(Players.Player1);
+            if (ratingForGame < 0)
+            {
+                Console.WriteLine("Game " + game.Player1.gamesCount + ". " + "Rating for game must be less then 0");
+                game.Player1.gamesCount--;
+                return;
+            }
+            this.rating -= ratingForGame / 2;
+            if(this.rating < 0)
+                this.rating = 1;
+            GameResults.Add(new GameHistory(game.Player1, game.Player2, false, ratingForGame));
+        }
+    }
+}
---
+++ Account/WinStreakAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    class WinStreakAccount : BasicAccount
+    {
+        private const int winStreakPoins = 15;
+        private int winStreak = 0;
+        public WinStreakAccount(string playerName) : base(playerName) { }
+        //win streak account receives bonus points when a player wins 3 or more times in a row
+        public override void WinGame(Game game)
+        {
+            gamesCount++;
+            winStreak++;
+            int ratingForGame;
+            if(winStreak >= 3) 
+            {
+                ratingForGame = winStreakPoins + game.GetRatingForGame(Players.Player1);
+                if (ratingForGame < 0)
+                {
+                    Console.WriteLine("Game " + gamesCount + ". " + "Rating for game must be less then 0");
+                    game.Player1.gamesCount--;
+                    return;
+                }
+                Console.WriteLine(game.Player1.playerName + " got 15 bonus points for win streak.");
+                GameResults.Add(new GameHistory(game.Player1, game.Player2, true, ratingForGame - winStreakPoins));
+            }
+            else
+            {
+                ratingForGame = game.GetRatingForGame(Players.Player1);
+                if (ratingForGame < 0)
+                {
+                    Console.WriteLine("Game " + game.Player1.gamesCount + ". " + "Rating for game must be less then 0");
+                    game.Player1.gamesCount--;
+                    return;
+                }
+                GameResults.Add(new GameHistory(game.Player1, game.Player2, true, ratingForGame));
+            }
+            gamesCount++;
+            this.rating += ratingForGame;
+            
+        }
+        //Also, win streak account loses half as many points (premium account privilege)
+        public override void LoseGame(Game game)
+        {
+            gamesCount++;
+            winStreak = 0;
+            int ratingForGame = game.GetRatingForGame(Players.Player1);
+            if (ratingForGame < 0)
+            {
+                Console.WriteLine("Game " + game.Player1.gamesCount + ". " + "Rating for game must be less then 0");
+                game.Player1.gamesCount--;
+                return;
+            }
+            this.rating -= ratingForGame / 2;
+            if (this.rating < 0)
+                this.rating = 1;
+            GameResults.Add(new GameHistory(game.Player1, game.Player2, false, ratingForGame));
+        }
+    }
+}
